38101d685171ab6f0a32915974cd74cc27027afe
Malik H <malik.h@webdevpro.net>
2022-01-10 17:30:10 +0100

    fin journée 1

--- jour1-tp/src/app/component/home.component.ts
+++ jour1-tp/src/app/component/home.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
 import { PostService } from "../service/post.service";
 import {Post} from "../service/post"
+import {trigger , animate , transition , keyframes, style} from "@angular/animations"
 @Component({
   selector: 'home',
   template: `
     <article *ngFor="let article of articles">
-      <h2>{{ article.title }}</h2>
+      <h2 @bouceIn>{{ article.title }}</h2>
       <p>{{ article.body }}</p>
       <a [routerLink]="['page', article.id , article.title]">lire la suite ... </a>
     </article>
   `,
+  animations : [
+    trigger("bouceIn" , [
+      transition("void => *" , [
+        animate(1000 , keyframes([
+          style({ offset : 0 , opacity: 0,
+            transform: "scale3d(0.3, 0.3, 0.3)"}),
+          style({ offset : 0.2 , transform: "scale3d(1.1, 1.1, 1.1)"}),
+          style({ offset : 0.4 , transform: "scale3d(0.9, 0.9, 0.9)"}),
+          style({ offset : 0.6 , opacity: 1 ,
+            transform: "scale3d(1.03, 1.03, 1.03)"} ),
+          style({ offset : 0.8 , transform: "scale3d(0.97, 0.97, 0.97)"}),
+          style({ offset : 1 , opacity: 1 , 
+            transform: "scale3d(1, 1, 1)"}),
+        ]))
+      ])
+    ])
+  ],
   styles: [`
     :host{
       display:grid;
