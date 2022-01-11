857095263f455669ed1e9af187fdb2d84bebf979
Malik H <malik.h@webdevpro.net>
2022-01-11 14:35:31 +0100

    correction

--- jour2-tp/src/app/resultat/resultat.component.ts
+++ jour2-tp/src/app/resultat/resultat.component.ts
@@ -1,15 +1,46 @@
 import { Component, OnInit } from '@angular/core';
 import { PartageService} from "../partage.service";
 import { mergeMap , pluck } from "rxjs/operators";
+import { Observable } from 'rxjs';
+
+/* interface interfaceCocktail {
+  strDrink
+} */
 
 @Component({
   selector: 'app-resultat',
-  template: '<pre>{{ p | json }}</pre>',
-  styleUrls: ['./resultat.component.css']
+  template : `
+    <article *ngFor="let cocktail of p">
+      <h2>{{ cocktail.strDrink}}</h2>
+      <img [src]="cocktail.strDrinkThumb" [alt]="cocktail.strDrink">
+      <p>{{ cocktail.strInstructions }}</p>
+      <ul [innerHTML]="ingrediants(cocktail)" ></ul>
+    </article>
+  `,
+  styles: [`
+    :host{
+      display:grid;
+      grid-template-columns: repeat(3,1fr);
+      gap : 20px;
+    }
+    img{
+      max-width:100%;
+    }
+  `]
 })
 export class ResultatComponent implements OnInit {
 
-  public p : string = "";
+  public p : any;
+  public ingrediants(cocktail : any) :string{
+    let resultat : string = "";
+    for(let i = 1 ; i <=15 ; i++){
+      if(cocktail["strIngredient"+i] && cocktail["strMeasure"+i]){
+        resultat += `<li>${cocktail["strIngredient"+i]} -- ${cocktail["strMeasure"+i]}</li>`
+      }
+    }
+    return resultat
+  }
+
   constructor(private service : PartageService) { }
 
   ngOnInit(): void {
@@ -25,7 +56,7 @@ export class ResultatComponent implements OnInit {
         // {drinks : [] , date : "1234556"}
         pluck( "drinks") // 
       )
-      .subscribe( (reponse :any) => this.p = reponse )
+      .subscribe( (reponse : any)  => this.p = reponse )
     
   }
 // https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita
