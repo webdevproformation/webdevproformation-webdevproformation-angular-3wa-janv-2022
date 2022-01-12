cba827be6a20489f268b165d687d67b89fc1616f
Malik H <malik.h@webdevpro.net>
2022-01-12 10:21:17 +0100

    form3

--- jour2-reactive-form/src/app/form3/form3.component.ts
+++ jour2-reactive-form/src/app/form3/form3.component.ts
@@ -7,15 +7,23 @@ import {FormGroup , FormControl , Validators , FormArray } from "@angular/forms"
     <h2>exemple3 : générer un formulaire depuis la class via FormArray</h2>
     <form [formGroup]="form" (ngSubmit)="onSubmit()">
       <div>
-        <input type="text" (keydown.enter)="addSujet(sujet)" #sujet>
+        <input type="text" #sujet>
+        <button (click)=addSujet(sujet)>ajouter un sujet</button>
       </div>
       <div *ngFor="let sujet of sujets()">
-        {{ sujet.value }}
+        {{ sujet.value }} <button (click)="supprSujet(sujet)">supprimer valeur</button>
       </div>
       <input type="submit">
     </form>`
 })
 export class Form3Component implements OnInit {
+
+  public supprSujet(sujet :any){
+      const form = this.form.get("sujets") as FormArray ;
+      const index = form.controls.indexOf(sujet) 
+      form.removeAt(index);   
+  }
+
   public addSujet(sujet : any){
     //console.log(value)
     (this.form.get("sujets") as FormArray).push( new FormControl(sujet.value))
@@ -24,7 +32,7 @@ export class Form3Component implements OnInit {
     // permet d'ajouter une nouveau champ dans sujets
   }
   public sujets (){
-    if(this.form.get('sujets') ){ 
+    if(this.form.get('sujets') ){ // vérifie que form contient bien la propriété sujet != null (narrowing)
       return (this.form.get('sujets') as any )['controls']
     }
   }
