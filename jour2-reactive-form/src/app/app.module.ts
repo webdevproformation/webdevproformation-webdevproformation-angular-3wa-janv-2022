da5b8c73879ade07ae2a83efd2f54f35b6c6b341
Malik H <malik.h@webdevpro.net>
2022-01-12 12:42:24 +0100

    initialisation de firebase !

--- jour2-reactive-form/src/app/app.module.ts
+++ jour2-reactive-form/src/app/app.module.ts
@@ -1,6 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { ReactiveFormsModule } from "@angular/forms";
+// identifiants 
+import { environment } from "../environments/environment"; // 
+
+// npm i firebase @angular/fire 
+import { AngularFireModule } from "@angular/fire/compat";
+import { AngularFireDatabaseModule } from "@angular/fire/compat/database"
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -24,7 +30,9 @@ import { Exo2Component } from './exo2/exo2.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireDatabaseModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- jour2-reactive-form/src/environments/environment.ts
+++ jour2-reactive-form/src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase : {
+    apiKey: "AIzaSyCKo5mWAl54I3HPjlTeTE5GkKeEbq1DM2E",
+    authDomain: "wa-demo-66613.firebaseapp.com",
+    databaseURL: "https://wa-demo-66613-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "wa-demo-66613",
+    storageBucket: "wa-demo-66613.appspot.com",
+    messagingSenderId: "193172438468",
+    appId: "1:193172438468:web:b19b637a9a8deb1fee7891"
+  }
 };
 
 /*
