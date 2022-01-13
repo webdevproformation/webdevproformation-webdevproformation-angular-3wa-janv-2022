a31cea3a2c556a621b3ca094fdf9e634449c4ad8
Malik H <malik.h@webdevpro.net>
2022-01-13 14:59:59 +0100

    affichage tableau

--- jour4-tp/src/app/back/gestion.component.ts
+++ jour4-tp/src/app/back/gestion.component.ts
@@ -1,20 +1,41 @@
 import { Component, OnInit } from '@angular/core';
+import { FirebaseService } from "../service/firebase.service"
 
 @Component({
   selector: 'gestion',
   template: `
-    <p>
-      gestion works!
-    </p>
+    <h1>gestion du site</h1>
+    <table mat-table [dataSource]="dataSource">
+    <ng-container matColumnDef="key">
+      <th mat-header-cell *matHeaderCellDef> key. </th>
+      <td mat-cell *matCellDef="let element"> {{element.key}} </td>
+    </ng-container>
+    <ng-container matColumnDef="prenom">
+      <th mat-header-cell *matHeaderCellDef> prenom. </th>
+      <td mat-cell *matCellDef="let element"> {{element.prenom}} </td>
+    </ng-container>
+    <ng-container matColumnDef="nom">
+      <th mat-header-cell *matHeaderCellDef> nom. </th>
+      <td mat-cell *matCellDef="let element"> {{element.nom}} </td>
+    </ng-container>
+    <tr mat-header-row *matHeaderRowDef="displayedColumns"></tr>
+    <tr mat-row *matRowDef="let row; columns: displayedColumns;"></tr>
+    </table>
   `,
   styles: [
+    `table{
+      width:100%;
+    }`
   ]
 })
 export class GestionComponent implements OnInit {
+  public displayedColumns: string[] = ['key', 'prenom', 'nom'];
+  public dataSource : Array<any> = []
 
-  constructor() { }
+  constructor(private fire : FirebaseService) { }
 
   ngOnInit(): void {
+    this.fire.getAll().subscribe( data => this.dataSource = data )
   }
 
 }
---
+++ jour4-tp/src/app/service/firebase.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { AngularFireDatabase } from '@angular/fire/compat/database';
+import {map} from "rxjs/operators";
+@Injectable({
+  providedIn: 'root'
+})
+export class FirebaseService {
+  private table : string = "/clients"
+  constructor(private db : AngularFireDatabase) { }
+  public getAll(){
+    return this.db.list(this.table).snapshotChanges().pipe( 
+      map( clients => 
+        clients.map( 
+          data => Object.assign( {}  , data.payload.val() , {key : data.key} )
+         ) 
+        )
+     )
+  }
+}
